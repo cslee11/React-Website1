c478b1b01a9f62c798d77527d8dc25d204fa72f7
cslee11 <75759362+cslee11@users.noreply.github.com>
2021-04-02 05:19:30 -0500

    completed to 22min

---
+++ src/components/Navbar.js
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom'
+
+function Navbar() {
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+    
+    return (
+        <>
+          <nav className='navbar'>
+            <div className='navbar-container'>
+                <Link to='/' className='navbar-logo'>
+                    TRVL <i className='fav fa-typo3' />
+                </Link>
+                <div className='menu-icon' onClick={handleClick}>
+                    <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                </div>
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                    <li className='nav-item'>
+                        <link to='/' className='nav-links' onClick={closeMobileMenu}>
+                        Home    
+                        </link> 
+                    </li>
+                </ul>
+            </div>
+          </nav>
+        </>
+    )
+}
+
+export default Navbar;
